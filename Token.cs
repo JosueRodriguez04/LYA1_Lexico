bc43534abbea8e5da80290de6db8189e88e45f42
JosueRodriguez04 <l21140777@queretaro.tecnm.mx>
2023-09-06 13:11:04 -0600

    first commit

---
+++ Token.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LYA1_Lexico
+{
+    public class Token
+    {
+        public enum Tipos
+        {
+            Identificador, Numero, Caracter, Asignacion, FinSentencia, Inicio, Fin,
+            OperadorRelacional, OperadorLogico, OperadorTermino, OperadorFactor,
+            IncrementoTermino, IncrementoFactor
+        }
+        private string contenido;
+        private Tipos clasificacion;
+        public Token()
+        {
+            contenido = "";
+            clasificacion = Tipos.Identificador;
+        }
+        public void setContenido(string contenido)
+        {
+            this.contenido = contenido;
+        }
+        public void setClasificacion(Tipos clasificacion)
+        {
+            this.clasificacion = clasificacion;
+        }
+        public string getContenido()
+        {
+            return this.contenido;
+        }
+        public Tipos getClasificacion()
+        {
+            return this.clasificacion;
+        }
+    }
+}
